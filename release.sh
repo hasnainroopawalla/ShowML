083c05a396ce2e6f531b33bd88c50f82a6ce15da
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2021-11-03 16:12:22 +0100

    Updated version number

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a v$(python3 setup.py --version) -m "Added Linear Regression, basic preprocessing and a data loader"
+git tag -a v$(python3 setup.py --version) -m "Added Logistic Regression"
 git push origin v$(python3 setup.py --version)
