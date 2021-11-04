0cb8249892f0cbd6e4eff84b468933a824987c55
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2021-11-04 09:17:24 +0100

    Updated release.sh

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-git tag -a v$(python3 setup.py --version) -m "Added Logistic Regression"
+git tag -a v$(python3 setup.py --version) -m "Added Logistic Regression and the Binary Cross Entropy Loss function"
 git push origin v$(python3 setup.py --version)
