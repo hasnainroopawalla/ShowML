b1274be05cec77acecb9ef297b21f99a5526afa8
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2021-12-07 11:21:21 +0100

    Added function defs to grid.py

--- showml/simulations/conways_game_of_life/grid.py
+++ showml/simulations/conways_game_of_life/grid.py
@@ -1,98 +1,49 @@
+import numpy as np
+
+
 class Grid:
-    def __init__(self, num_rows, num_columns) -> None:
+    """A 2D Grid class to display the cells.
+    """
+
+    def __init__(self, num_rows: int, num_columns: int) -> None:
+        """[summary]
+
+        Args:
+            num_rows (int): The number of rows in the grid.
+            num_columns (int): The number of columns in the grid.
+        """
         self.num_rows = num_rows
         self.num_columns = num_columns
-        self.grid = [[0 for _ in range(num_columns)] for _ in range(num_rows)]
+        self.grid: np.ndarray = np.zeros((num_rows, num_columns))
 
         # Set initial live cells
-        self.grid[2][3] = 1
-        self.grid[3][3] = 1
-        self.grid[3][4] = 1
-        self.grid[2][4] = 1
+        self.grid[9][11] = 1
+        self.grid[10][11] = 1
+        self.grid[11][11] = 1
+        self.grid[10][12] = 1
+        self.grid[10][10] = 1
 
-        self.grid[4][5] = 1
-        self.grid[5][5] = 1
-        self.grid[4][6] = 1
-        self.grid[5][6] = 1
+    def calculate_neighbor_sum(self, row: int, column: int) -> int:
+        """This method computes the sum of the 8-neighbors of the cell at [row, column].
+        Reference: https://stackoverflow.com/a/37026344
 
-    def calculate_neighbor_sum(self, row, column):
-        if row == 0 and column == 0:
-            neighbor_sum = (
-                self.grid[row + 1][column]
-                + self.grid[row][column + 1]
-                + self.grid[row + 1][column + 1]
-            )
-        elif row == 0 and column == self.num_columns - 1:
-            neighbor_sum = (
-                self.grid[row][column - 1]
-                + self.grid[row + 1][column]
-                + self.grid[row + 1][column - 1]
-            )
-        elif row == 0:
-            neighbor_sum = (
-                self.grid[row + 1][column]
-                + self.grid[row][column + 1]
-                + self.grid[row][column - 1]
-                + self.grid[row + 1][column - 1]
-                + self.grid[row + 1][column + 1]
-            )
+        Args:
+            row (int): The row index.
+            column (int): The column index.
 
-        elif column == 0 and row == self.num_rows - 1:
-            neighbor_sum = (
-                self.grid[row - 1][column]
-                + self.grid[row][column + 1]
-                + self.grid[row - 1][column + 1]
-            )
-        elif column == 0:
-            neighbor_sum = (
-                self.grid[row - 1][column]
-                + self.grid[row][column + 1]
-                + self.grid[row + 1][column]
-                + self.grid[row - 1][column + 1]
-                + self.grid[row + 1][column + 1]
-            )
+        Returns:
+            int: Sum of the 8 neighbors of the cell.
+        """
+        # Extract the region (8-neighbors and self) of element at [row, column]
+        region = self.grid[max(0, row - 1) : row + 2, max(0, column - 1) : column + 2]
+        # Sum the region and subtract center
+        return np.sum(region) - self.grid[row, column]
 
-        elif column == self.num_columns - 1 and row == self.num_rows - 1:
-            neighbor_sum = (
-                self.grid[row - 1][column]
-                + self.grid[row][column - 1]
-                + self.grid[row - 1][column - 1]
-            )
-
-        elif row == self.num_rows - 1:
-            neighbor_sum = (
-                self.grid[row - 1][column]
-                + self.grid[row][column + 1]
-                + self.grid[row][column - 1]
-                + self.grid[row - 1][column - 1]
-                + self.grid[row - 1][column + 1]
-            )
-
-        elif column == self.num_columns - 1:
-            neighbor_sum = (
-                self.grid[row][column - 1]
-                + self.grid[row - 1][column]
-                + self.grid[row + 1][column]
-                + self.grid[row - 1][column - 1]
-                + self.grid[row + 1][column - 1]
-            )
-
-        else:
-            neighbor_sum = (
-                self.grid[row - 1][column - 1]
-                + self.grid[row - 1][column]
-                + self.grid[row - 1][column + 1]
-                + self.grid[row][column - 1]
-                + self.grid[row][column + 1]
-                + self.grid[row + 1][column - 1]
-                + self.grid[row + 1][column]
-                + self.grid[row + 1][column + 1]
-            )
-
-        return neighbor_sum
-
-    def update_grid(self):
-        new_grid = [[0 for _ in range(self.num_columns)] for _ in range(self.num_rows)]
+    def update_grid(self) -> None:
+        """This method updates the grid using the rules of Conway's Game of Life.
+        Reference: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+        """
+        new_grid = np.zeros((self.num_rows, self.num_columns))
         for row in range(self.num_rows):
             for column in range(self.num_columns):
                 neighbor_sum = self.calculate_neighbor_sum(row, column)
@@ -108,8 +59,16 @@ class Grid:
                         new_grid[row][column] = 0
         self.grid = new_grid
 
-    def reset_grid(self):
-        self.grid = [[0 for x in range(self.num_columns)] for y in range(self.num_rows)]
+    def reset_grid(self) -> None:
+        """This method resets the grid i.e., kills all the cells.
+        """
+        self.grid = np.zeros((self.num_rows, self.num_columns))
 
-    def toggle_cell_value(self, row, column):
+    def toggle_cell_value(self, row: int, column: int) -> None:
+        """This method toggles the value of a cell (sets to 1 if 0 else 0)
+
+        Args:
+            row (int): The row index.
+            column (int): The column index.
+        """
         self.grid[row][column] = 1 if self.grid[row][column] == 0 else 0
