488b11f11ef0338cc6193a35c1273200d96d00bc
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-01-14 18:08:48 +0100

    Minor update to docstring

--- showml/utils/plots.py
+++ showml/utils/plots.py
@@ -19,7 +19,7 @@ def generic_metric_plot(metric_name: str, metric_values: List[float]) -> None:
 
 def plot_actual_vs_predicted(y: np.ndarray, z: np.ndarray) -> None:
     """Generates a scatter plot of the true values and predicted values.
-    A diagonal line from (0, 0) to (+limit, +limit) indicates a very good fit i.e., the true values and predicted values are almost the same.
+    A diagonal line from (0, 0) to (+limit, +limit) indicates a very good fit i.e., the true values and predicted values are almost equal.
 
     Args:
         y (np.ndarray): The true labels of the input data.
